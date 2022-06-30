efc04fba393338cbe8cc37d72b7468bec1139c49
Amnaaaa_ <amna.riaz@invozone.com>
2022-06-30 11:13:46 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
+root.render(<App />);
+
+reportWebVitals();
